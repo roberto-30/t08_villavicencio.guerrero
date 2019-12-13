57c451240849f053cab3ad7879b216c65abea7e8
roberto-30 <56975848+roberto-30@users.noreply.github.com>
2019-12-13 13:49:42 -0500

    Add files via upload

---
+++ comparacion.py
@@ -0,0 +1,76 @@
+#ejemplo 01
+
+str_1="CHICLAYO"
+msg_1=("FEREÑAFE")
+
+str_1==msg_1
+print()
+#ejemplo 02
+
+msg_2=" LA CIUDAD"
+print(msg_2)
+
+#ejemplo 03
+
+msg_3="DE LA AMISTAD"
+print(msg_3)
+
+#ejemplo 04
+
+msg_4=" SU DEPARTAMENTO"
+print(msg_4)
+
+#ejemplo 05
+
+msg_5=" ES LAMBAYEQUE"
+print(msg_5)
+
+#ejemplo 06
+
+msg_6=" WINDOWS"
+print(msg_6)
+
+#ejemplo 07
+
+msg_7=" UN SISTEMA"
+print(msg_7)
+
+#ejemplo 08
+
+msg_8=" OPERATIVO"
+print(msg_8)
+
+#ejemplo 09
+
+msg_9=" CREADO POR MICROSOFT"
+print(msg_9)
+
+#ejemplo 10
+
+msg_10=" ROBERTO"
+print(msg_10)
+
+#ejemplo 11
+
+msg_11=" CARLOS"
+print(msg_11)
+
+#ejemplo 12
+
+msg_12=" GUERRERO"
+print(msg_12)
+
+#ejemplo 13
+
+msg_13=" GUERRERO"
+print(msg_13)
+
+#ejemplo 14
+
+msg_14=" YOHANA"
+print(msg_14)
+
+#ejemplo 15
+
+msg_15=" PARAGUAY"
+print(msg_15)
